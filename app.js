d701dfcf21e3f8e6db346e5d29b9e130a24351b3
Advith Chelikani <advith.chelikani@gmail.com>
2015-06-08 19:05:18 -0700

    maps and profile form added

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
+// Including Express.js and Stormpath
 var express = require('express');
 var stormpath = require('express-stormpath');
 
 var app = express();
 
+// Setting up jade as the view engine
 app.set('views', './views');
 app.set('view engine', 'jade');
 
+// Using my stormpath credentials to intialize stormpath middleware
+// More params can be added for more customization
 var stormpathMiddleware = stormpath.init(app, {
   apiKeyFile: 'apiKey.properties',
   application: 'https://api.stormpath.com/v1/applications/5WbefiZ9jkZkusio1ujUOq',
@@ -16,18 +20,19 @@ var stormpathMiddleware = stormpath.init(app, {
 
 app.use(stormpathMiddleware);
 
+// Rendering home page
 app.get('/', function(req, res) {
   res.render('home', {
     title: 'Welcome'
   });
 });
 
-app.get('/profile', function(req, res) {
-   res.render('profile', {
-	 title: 'Profile'
-   });
+app.get('/map', function(req, res) {
+  res.render('map', {
+    title: 'Map'
+  });
 });
 
-
+app.use('/profile',stormpath.loginRequired,require('./profile')());
 
 app.listen(5000);
